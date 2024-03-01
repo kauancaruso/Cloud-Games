ad15f6961beebb564276b397fa50113c0525c058
Kauan Caruso <123602468+kauancaruso@users.noreply.github.com>
2024-02-29 22:04:27 -0300

    create new function - format currency

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,24 @@ var linhasTabela = document.querySelectorAll(".linhas");
 for(var i = 0; i < linhasTabela.length; i++) {
 
     var quantidade = linhasTabela[i].querySelector(".info-qtd").textContent;
-    var valoruni = linhasTabela[i].querySelector(".info-valor").textContent;
+    var valoruni = parseFloat(linhasTabela[i].querySelector(".info-valor").textContent);
 
     //Valida a quantidade
     if(quantidade < 1 || isNaN(quantidade)){
         //Quantidade NOK --- Reportar ao usuário
+        linhasTabela[i].querySelector(".info-valor").textContent = formValorMonetario(valoruni);
         linhasTabela[i].querySelector(".info-qtd").textContent = "QUANTIDADE INVÁLIDA!";
         linhasTabela[i].querySelector(".info-qtd").style.color = "red";
     }else{
         //Quantidade OK --- Prosseguir
-        if(isNaN(valoruni)){
+        if(valoruni <= 0 || isNaN(valoruni)){
             //Valor NOK --- Reportar ao usuário
             linhasTabela[i].querySelector(".info-valor").textContent = "VALOR INVÁLIDO!";
             linhasTabela[i].style.backgroundColor = "red";
         }else{
             //Valor OK --- Prosseguir
-            linhasTabela[i].querySelector(".info-total").textContent = "R$ " + calcularTotal(quantidade,valoruni);
+            linhasTabela[i].querySelector(".info-valor").textContent = formValorMonetario(valoruni);
+            linhasTabela[i].querySelector(".info-total").textContent = formValorMonetario(calcularTotal(quantidade,valoruni));
         }
     }
 }
@@ -31,6 +33,15 @@ for(var i = 0; i < linhasTabela.length; i++) {
 function calcularTotal(quantidade,valoruni){
     var total = 0;
     total = quantidade * valoruni;;
-
+    
     return total;
+}
+
+//Formatando valores monetarios
+function formValorMonetario(valor){
+    var formReal = 0;
+
+    formReal = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    return formReal;
 }
